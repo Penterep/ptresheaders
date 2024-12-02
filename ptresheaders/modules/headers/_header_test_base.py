9f8619157721d3f1a8c646a1bf2b1acb2a12f470
Daniel <d.kummel@penterep.com>
2024-12-02 15:17:36 +0100

    Add response parameter, handle duplicates

--- ptresheaders/modules/headers/_header_test_base.py
+++ ptresheaders/modules/headers/_header_test_base.py
@@ -1,4 +1,4 @@
-from ptlibs.ptprinthelper import ptprint
+from ptlibs.ptprinthelper import ptprint, out_if, get_colored_text
 
 class HeaderTestBase:
     """
@@ -8,7 +8,7 @@ class HeaderTestBase:
     should inherit from this base class and implement the `test_header` method.
     """
 
-    def __init__(self, ptjsonlib, args, header_name, header_value):
+    def __init__(self, ptjsonlib, args, header_name, header_value, response, is_duplicate):
         """
         Initialize the base class for header tests.
 
@@ -25,8 +25,12 @@ class HeaderTestBase:
         self.args = args
         self.header_name = header_name
         self.header_value = header_value
+        self.response = response
         ptprint(f"{header_name}:", "INFO", not self.args.json, colortext=True, newline_above=True)
+        if is_duplicate:
+            ptprint(get_colored_text("Duplicate header", "WARNING"), "TEXT", condition=not self.args.json, indent=4)
         ptprint(f"{self.header_name}: {self.header_value}", "ADDITIONS", not self.args.json, colortext=True, indent=4)
+
 
     def test_header(self):
         raise NotImplementedError("Not implemented")
